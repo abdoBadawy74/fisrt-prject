97907f3b8b8271b9b6473f0c5d82f8cd2e22a84f
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2026-08-05 02:17:29 +0300

    ActivityIndicator

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,19 @@
-import { StatusBar, View } from "react-native";
+import { useState } from "react";
+import { ActivityIndicator, Button, StatusBar, View } from "react-native";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <StatusBar barStyle="dark-content" backgroundColor={"#313030"} hidden />
+      <ActivityIndicator
+        style={{ marginBottom: 20 }}
+        size="large"
+        color="#707070"
+        animating={isLoading}
+      />
+      <StatusBar barStyle="dark-content" />
+
+      <Button title="Load Data" onPress={() => setIsLoading(!isLoading)} />
     </View>
   );
 }
