570f839ba5f7db14390fb1f1e29cc477de9b24b1
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2026-08-05 01:47:27 +0300

    ImageBackground

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
-import { Image, Text, View } from "react-native";
+import { ImageBackground } from "expo-image";
+import { Text, View } from "react-native";
 export default function App() {
   return (
     <View
@@ -13,17 +14,12 @@ export default function App() {
         Hello <Text style={{ color: "orange" }}>World</Text>
       </Text>
 
-      <Image
-        source={require("@/assets/images/icon.png")}
-        style={{ width: 50, height: 50, marginTop: 20 }}
-      ></Image>
-
-      <Image
+      <ImageBackground
         source={{
           uri: "https://images.unsplash.com/photo-1785370145074-918866a3986b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
         }}
         style={{ width: 50, height: 50, marginTop: 20 }}
-      ></Image>
+      ></ImageBackground>
     </View>
   );
 }
