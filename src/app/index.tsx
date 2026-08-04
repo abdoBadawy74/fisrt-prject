27b7802974f6a37eace4002232fb351dcbfd69a5
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2026-08-05 01:57:48 +0300

    Pressable

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,20 @@
-import { Button, ScrollView } from "react-native";
+import { Pressable, Text, View } from "react-native";
+
 export default function App() {
   return (
-    <ScrollView>
-      <Button title="Click me" onPress={() => alert("Button pressed!")} />
-    </ScrollView>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Pressable onPress={() => console.log("Pressed!")}>
+        <Text>Hello, World!</Text>
+      </Pressable>
+
+      <Pressable
+        onPressIn={() => console.log("Press In!")}
+        onPressOut={() => console.log("Press Out!")}
+        onLongPress={() => console.log("Long Pressed!")}
+        style={{ marginTop: 20 }}
+      >
+        <Text>Press Me!</Text>
+      </Pressable>
+    </View>
   );
 }
