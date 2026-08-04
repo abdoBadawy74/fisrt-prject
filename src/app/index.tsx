a996604d0cb366c605dbcf38cc1e73118d945aad
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2026-08-05 01:47:07 +0300

    Images

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,29 @@
-import { Text, View, StyleSheet } from "react-native";
-
-export default function Index() {
+import { Image, Text, View } from "react-native";
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+      }}
+    >
+      <Text style={{ fontSize: 20, color: "#000" }}>
+        Hello <Text style={{ color: "orange" }}>World</Text>
+      </Text>
+
+      <Image
+        source={require("@/assets/images/icon.png")}
+        style={{ width: 50, height: 50, marginTop: 20 }}
+      ></Image>
+
+      <Image
+        source={{
+          uri: "https://images.unsplash.com/photo-1785370145074-918866a3986b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
+        }}
+        style={{ width: 50, height: 50, marginTop: 20 }}
+      ></Image>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
