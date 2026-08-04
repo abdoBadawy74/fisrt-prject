6e1236a35a1f549d43dfdde9a564e583ee6af5f4
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2026-08-05 01:48:11 +0300

    ScrollView

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,40 @@
 import { ImageBackground } from "expo-image";
-import { Text, View } from "react-native";
+import { ScrollView, Text } from "react-native";
 export default function App() {
   return (
-    <View>
+    <ScrollView>
       <ImageBackground
         source={{
           uri: "https://images.unsplash.com/photo-1785370145074-918866a3986b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
         }}
-        style={{ width: 300, height: 300, marginTop: 20 }}
+        style={{ width: 500, height: 500, marginTop: 20 }}
       >
         <Text>
-          Hello <Text style={{ color: "orange" }}>World</Text>
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </Text>
       </ImageBackground>
-    </View>
+      <ImageBackground
+        source={{
+          uri: "https://images.unsplash.com/photo-1785370145074-918866a3986b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
+        }}
+        style={{ width: 500, height: 500, marginTop: 20 }}
+      >
+        <Text>
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+      </ImageBackground>
+    </ScrollView>
   );
 }
