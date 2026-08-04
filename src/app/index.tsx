2e6b11d7bc1d3214a3ae475dcb417511a5cb571a
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2026-08-05 02:05:06 +0300

    StatusBar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,9 @@
-import { Pressable, Text, View } from "react-native";
+import { StatusBar, View } from "react-native";
 
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Pressable onPress={() => console.log("Pressed!")}>
-        <Text>Hello, World!</Text>
-      </Pressable>
-
-      <Pressable
-        onPressIn={() => console.log("Press In!")}
-        onPressOut={() => console.log("Press Out!")}
-        onLongPress={() => console.log("Long Pressed!")}
-        style={{ marginTop: 20 }}
-      >
-        <Text>Press Me!</Text>
-      </Pressable>
+      <StatusBar barStyle="dark-content" backgroundColor={"#313030"} hidden />
     </View>
   );
 }
