2b1855632cb975361427106e50f30dc219b598e9
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2026-08-05 02:25:43 +0300

    alert

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
-import { ActivityIndicator, Button, StatusBar, View } from "react-native";
+import { Alert, Button, View } from "react-native";
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true);
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <ActivityIndicator
-        style={{ marginBottom: 20 }}
-        size="large"
-        color="#707070"
-        animating={isLoading}
+      <Button
+        title="Delete my account"
+        onPress={() =>
+          Alert.alert("Are you sure?", "This action cannot be undone.", [
+            { text: "Cancel", style: "cancel" },
+            {
+              text: "Delete",
+              style: "destructive",
+              onPress: () => console.log("Account deleted"),
+            },
+          ])
+        }
       />
-      <StatusBar barStyle="dark-content" />
-
-      <Button title="Load Data" onPress={() => setIsLoading(!isLoading)} />
     </View>
   );
 }
