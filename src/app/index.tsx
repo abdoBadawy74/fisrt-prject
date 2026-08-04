8575faeaef50390cd8a0e695ff92f3fcc6ab1ed7
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2026-08-05 01:48:28 +0300

    Buttons

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,40 +1,8 @@
-import { ImageBackground } from "expo-image";
-import { ScrollView, Text } from "react-native";
+import { Button, ScrollView } from "react-native";
 export default function App() {
   return (
     <ScrollView>
-      <ImageBackground
-        source={{
-          uri: "https://images.unsplash.com/photo-1785370145074-918866a3986b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
-        }}
-        style={{ width: 500, height: 500, marginTop: 20 }}
-      >
-        <Text>
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
-      </ImageBackground>
-      <ImageBackground
-        source={{
-          uri: "https://images.unsplash.com/photo-1785370145074-918866a3986b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
-        }}
-        style={{ width: 500, height: 500, marginTop: 20 }}
-      >
-        <Text>
-          lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
-      </ImageBackground>
+      <Button title="Click me" onPress={() => alert("Button pressed!")} />
     </ScrollView>
   );
 }
