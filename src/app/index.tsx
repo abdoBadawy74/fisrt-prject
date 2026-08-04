f9962be0e559b98fcecf520e35d016ae0a04362d
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2026-08-05 01:47:55 +0300

    ImageBackground with text

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,24 +2,17 @@ import { ImageBackground } from "expo-image";
 import { Text, View } from "react-native";
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#fff",
-      }}
-    >
-      <Text style={{ fontSize: 20, color: "#000" }}>
-        Hello <Text style={{ color: "orange" }}>World</Text>
-      </Text>
-
+    <View>
       <ImageBackground
         source={{
           uri: "https://images.unsplash.com/photo-1785370145074-918866a3986b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
         }}
-        style={{ width: 50, height: 50, marginTop: 20 }}
-      ></ImageBackground>
+        style={{ width: 300, height: 300, marginTop: 20 }}
+      >
+        <Text>
+          Hello <Text style={{ color: "orange" }}>World</Text>
+        </Text>
+      </ImageBackground>
     </View>
   );
 }
